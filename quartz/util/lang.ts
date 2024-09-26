e050f302c22b9256c6d40aa4b8eb1566f04380b7
DHintz137 <dhintz1@uwyo.edu>
2024-09-26 16:59:51 -0600

    Add OverlayExplorer component

--- quartz/util/lang.ts
+++ quartz/util/lang.ts
@@ -9,3 +9,8 @@ export function pluralize(count: number, s: string): string {
 export function capitalize(s: string): string {
   return s.substring(0, 1).toUpperCase() + s.substring(1)
 }
+
+// Add the classNames function
+export function classNames(...classes: (string | false | null | undefined)[]): string {
+  return classes.filter(Boolean).join(' ');
+}
